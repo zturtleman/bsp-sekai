528a613fd446a7a661207f46727c01a4ab973ffb
Zack Middleton <zturtleman@gmail.com>
2015-07-28 17:55:04 -0500

    Add GPL headers to new files for BSP sekai

--- code/sekai.h
+++ code/sekai.h
@@ -1,4 +1,32 @@
-// GPLv2+
+/*
+===========================================================================
+Copyright (C) 2015 Zack Middleton
+
+This file is part of BSP sekai Source Code.
+
+BSP sekai Source Code is free software; you can redistribute it
+and/or modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 3 of the License,
+or (at your option) any later version.
+
+BSP sekai Source Code is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BSP sekai Source Code.  If not, see <http://www.gnu.org/licenses/>.
+
+In addition, BSP sekai Source Code is also subject to certain additional terms.
+You should have received a copy of these additional terms immediately following
+the terms and conditions of the GNU General Public License.  If not, please
+request a copy in writing from id Software at the address below.
+
+If you have questions concerning this license or the applicable additional
+terms, you may contact in writing id Software LLC, c/o ZeniMax Media Inc.,
+Suite 120, Rockville, Maryland 20850 USA.
+===========================================================================
+*/
 
 #include <stdlib.h>
 #include <stdint.h>
